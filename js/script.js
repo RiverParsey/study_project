449d9220d75c6c2ce6baef8915f735cb0a8109a9
richard <rihardptrk18@gmail.com>
2023-03-21 16:53:37 +0100

    nested loops

--- js/script.js
+++ js/script.js
@@ -117,9 +117,67 @@
 // Next lesson : module1-task6
 // Циклы
 
-let snum = 50;
+// let num = 50;
 
-while (num < 55) {
-  console.log(num);
-  num ++;
+// while (num <= 55) {
+//   console.log(num);
+//   num ++;
+// }
+
+// do {
+//   console.log(num);
+//   num ++;
+// }
+// while (num < 55);
+
+// for (let i = 1; i < 10;  i++) {
+//   if(i === 6) {
+//     // break;
+//     continue;
+//   }
+
+//   console.log(i);
+// }
+
+
+// Next lesson : module1-task7
+// Циклы в цикле и метки
+
+for (let i = 0; i < 3; i++) {
+  console.log(i);
+  for (let j = 0; j < 3; j++) {
+    console.log(j);
+  }
+}
+
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+
+let result = '';
+const length = 7;
+
+for (let i = 1; i < length; i++) {
+
+  for (let j = 0; j < i;  j++) {
+    result += "*";
+  }
+
+  result += '\n';
+}
+
+console.log(result);
+
+first: for (let i = 0; i < 3; i++) {
+  console.log(`First level: ${i}`);
+  for (let j = 0; j < 3; j++) {
+    console.log(`Second level: ${j}`);
+    for (let k = 0; k < 5; k++) {
+      if (k === 2) break first;
+      console.log(`Third level: ${k}`);
+    }
+  }
 }
