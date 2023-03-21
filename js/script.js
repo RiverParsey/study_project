600344be939e67d8d39529da8ca1420e94461b01
richard <rihardptrk18@gmail.com>
2023-03-21 15:04:35 +0100

    loops

--- js/script.js
+++ js/script.js
@@ -63,14 +63,63 @@
 // Next lesson : module1-task5
 // Логические операторы
 
-//&&    Логическое И
-//||    Логическое ИлИ
+// &&    Логическое И
+// ||    Логическое ИлИ
+// !     Оператор Не
 
-const hamburger = true;
-const fries = false;
+// const hamburger = 5;
+// const fries = 0;
 
-if (hamburger && fries) {
-  console.log('Я сыт!');
+// if (hamburger && fries) {
+//   console.log('Я сыт!');
+// }
+
+// const hamburger = 3;
+// const fries = 1;
+// const cola = 1;
+
+// console.log(hamburger === 3 && cola && fries);
+
+// console.log(1 && 0);
+// console.log(1 && 5);
+// console.log(null && 5);
+// console.log(0 && 'yo bitch');
+
+// if (hamburger === 3 && cola === 1 && fries) {
+//   console.log('Все сыты!');
+// } else {
+//   console.log('Мы уходим');
+// }
+
+// Оператор И всегда возвращает первое ложное значение если оно есть, дальше него код уже не идет
+// Если все аргументы верны то возвращается значение последнего аргумента
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+// if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
+//   console.log('Все довольны!');
+// } else {
+//   console.log('Мы уходим');
+// }
+
+// console.log(hamburger === 3 && cola === 2 || fries === 3 && nuggets);
+
+// let johnReport, alexReport, samReport, mariaReport = 'done';
+
+// console.log(johnReport || alexReport || samReport || mariaReport);
+
+// console.log(!0);
+
+
+// Next lesson : module1-task6
+// Циклы
+
+let snum = 50;
+
+while (num < 55) {
+  console.log(num);
+  num ++;
 }
-
-console.log(hamburger && fries);
