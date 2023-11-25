60d59a3c066b0d0b0cf34dcba5b4bb90e5e3b63f
Richard Cheese <richardcheese@Richards-MacBook-Pro.local>
2023-11-25 14:36:05 +0100

    module1-task21

--- js/script.js
+++ js/script.js
@@ -649,25 +649,25 @@
 // Next lesson : module1-task15
 // Объекты, деструктуризация объектов
 
-// const options = {
-//   name: 'test',
-//   width: 1024,
-//   height: 1024,
-//   colors: {
-//     border: 'black',
-//     bg: 'red'
-//   }
-// };
+const options = {
+  name: 'test',
+  width: 1024,
+  height: 1024,
+  colors: {
+    border: 'black',
+    bg: 'red'
+  }
+};
 
-// for (let key in options) {
-//   if (typeof(options[key]) === 'object') {
-//     for (let i in options[key]) {
-//       console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
-//     }
-//   } else {
-//       console.log(`Свойство ${key} имеет значение ${options[key]}`);
-//   }
-// }
+for (let key in options) {
+  if (typeof(options[key]) === 'object') {
+    for (let i in options[key]) {
+      console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+    }
+  } else {
+      console.log(`Свойство ${key} имеет значение ${options[key]}`);
+  }
+}
 
 
 // Next lesson : module1-task16
