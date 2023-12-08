9ef1e3bcb7f25d52d0ed5b5151c3c74796ea446b
Richard Cheese <richardcheese@Richards-MacBook-Pro.local>
2023-12-08 14:52:12 +0100

    module1-task34

--- js/script.js
+++ js/script.js
@@ -1308,3 +1308,59 @@
 // module1-task33  отлавливаем ошибки в коде
 
 // lesson 38 отлавливание ошибок в консоле разработчика. Breakpoints
+
+
+
+// module1-task34 динамическая типизация в JS
+
+// // to string
+
+// // 1)
+// console.log(String(null));
+// console.log(typeof(String(4)));
+
+// // 2)
+// console.log(typeof(null + ''));
+
+// const num = 5;
+
+// console.log('https://vk.com/catalog/' + num);
+
+// const fontSize = 26 + 'px';
+
+// // to number
+
+// // 1)
+// console.log(typeof(Number('4')));
+
+// // 2)
+// console.log(typeof(+'5'));
+
+// // 3)
+// console.log(typeof(parseInt('15px', 10)));
+
+
+// let answ = +prompt('Hello', '');
+
+// // to boolean
+
+// // 0, '', null, undefined, NaN;  always false
+
+// // 1)
+// let switcher = null;
+
+// if (switcher) {
+//   console.log('working...');
+// }
+
+// switcher = 1;
+
+// if (switcher) {
+//   console.log('working...');
+// }
+
+// // 2)
+// console.log(typeof(Boolean('4')));
+
+// // 3)
+// console.log(typeof(!!'4444'));
