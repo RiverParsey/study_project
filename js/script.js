244793cb4d11f39837c83ef689fd37a1b8427833
Richard Cheese <richardcheese@Richards-MacBook-Pro.local>
2024-02-10 11:11:07 +0100

    module1-task58

--- js/script.js
+++ js/script.js
@@ -2404,3 +2404,69 @@
 // }
 
 // deleteElement();
+
+
+// module1-task58 WeakSet and WeakMap
+
+// let user = {
+//   name: 'ivan'
+// };
+
+// // const arr = [user]; // пока этот массив существует данные лежат в памяти
+
+// let map = new Map();
+// map.set(user, 'data');
+
+// user = null;
+
+// console.log(map.keys());
+
+// console.log(user);
+// console.log(arr[0]);
+
+// let user = {name: 'ivan'};
+
+// let map = new WeakMap();
+// map.set(user, 'data');
+
+// user = null;
+
+// console.log(map.has(user));
+
+// let cache = new WeakMap();
+
+// function cacheUser(user) {
+//   if (!cache.has(user)) {
+//     cache.set(user, Date.now());
+//   }
+
+//   return cache.get(user);
+// }
+
+// let lena = {name: 'elena'};
+// let alex = {name: 'alex'};
+
+// cacheUser(lena);
+// cacheUser(alex);
+
+// lena = null;
+
+// console.log(cache.has(lena));
+// console.log(cache.has(alex));
+
+// WeakSet
+// add(), has(), delete()
+
+// let messages = [
+//   {text: 'hello', from: 'john'},
+//   {text: 'world', from: 'alex'},
+//   {text: '...', from: 'M'}
+// ];
+
+// let readMessages = new WeakSet();
+
+// readMessages.add(messages[0]);
+// readMessages.add(messages[1]);
+
+// messages.shift();
+// console.log(readMessages.has(messages[0]));
