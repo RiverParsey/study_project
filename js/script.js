3c7571440fe7b3e2c080069736b6cc1fe9010f75
Richard Cheese <richardcheese@Richards-MacBook-Pro.local>
2024-02-16 12:24:06 +0100

    module1-task64

--- js/script.js
+++ js/script.js
@@ -2729,3 +2729,52 @@
 //     closeModal();
 //   }
 // }));
+
+
+// module1-task64 модификации модального окна
+
+// const modalTrigger = document.querySelectorAll('[data-modal]'),
+//       modal = document.querySelector('.modal'),
+//       modalCloseBtn = document.querySelector('[data-close]');
+
+// function openModal() {
+//   modal.classList.add('show');
+//   modal.classList.remove('hide');
+//   document.body.style.overflow = 'hidden';
+//   clearInterval(modalTimerId);
+// };
+
+// modalTrigger.forEach(btn => {
+//   btn.addEventListener('click', openModal);
+// });
+
+// function closeModal() {
+//   modal.classList.add('hide');
+//   modal.classList.remove('show');
+//   document.body.style.overflow = '';
+// };
+
+// modalCloseBtn.addEventListener('click', closeModal);
+
+// modal.addEventListener('click', (e) => {
+//   if (e.target === modal) {
+//     closeModal();
+//   }
+// });
+
+// document.addEventListener('keydown', (e) => {
+//   if (e.code === 'Escape' && modal.classList.contains('show')) {
+//     closeModal();
+//   }
+// });
+
+// const modalTimerId = setTimeout(openModal, 3000);
+
+// function showModalByScroll() {
+//   if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+//     openModal();
+//     window.removeEventListener('scroll', showModalByScroll);
+//   }
+// }
+
+// window.addEventListener('scroll', showModalByScroll);
