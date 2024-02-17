32e0c341940b2fa29d597df9cbed029a5ed085b7
Richard Cheese <richardcheese@Richards-MacBook-Pro.local>
2024-02-17 12:12:42 +0100

    module1-task66

--- js/script.js
+++ js/script.js
@@ -2793,3 +2793,34 @@
 // observer.observe(box, {
 //   childList: true
 // })
+
+
+
+// module1-task66 функции конструкторы
+
+// const num = new Number(3);
+// console.log(num);
+
+// function User(name, id) { // функция конструктор
+//   this.name = name;
+//   this.id = id;
+//   this.human = true;
+//   this.hello = function() {
+//     console.log(`hello ${this.name}`);
+//   }
+// }
+
+// User.prototype.exit = function() {
+//   console.log(`user ${this.name} quit`)
+// };
+
+// const ivan = new User('Ivan', 28);
+// const alex = new User('Alex', 20);
+
+// ivan.exit();
+
+// ivan.hello();
+// alex.hello();
+
+// console.log(ivan);
+// console.log(alex);
