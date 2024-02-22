0f239dc5cf89477bc68a770513affc57388b8371
Richard Cheese <richardcheese@Richards-MacBook-Pro.local>
2024-02-22 11:44:26 +0100

    module1-task69

--- js/script.js
+++ js/script.js
@@ -3040,3 +3040,41 @@
 // }
 
 // obj.sayNumber();
+
+
+
+// module1-task69 // классы ES6
+
+// class Rectangle {
+//   constructor(height, width) {
+//     this.height = height;
+//     this.width = width;
+//   }
+
+//   calcArea() {
+//     return this.height * this.width;
+//   }
+// }
+
+// class ColoredRectangleWithText extends Rectangle {
+//   constructor(height, width, text, bgColor) {
+//     super(height, width);
+//     this.text = text;
+//     this.bgColor = bgColor;
+//   }
+
+//   showMyProps() {
+//     console.log(`text: ${this.text}, color: ${this.bgColor}`)
+//   }
+// }
+
+// const div = new ColoredRectangleWithText(25, 10, 'hello world', 'red');
+
+// div.showMyProps();
+// console.log(div.calcArea());
+
+// const square = new Rectangle(10, 10);
+// const long = new Rectangle(20, 100);
+
+// console.log(square.calcArea());
+// console.log(long.calcArea());
