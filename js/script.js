00bb0c9e6d7a894bae82ddbc097ab1555249b9f1
Richard Cheese <richardcheese@Richards-MacBook-Pro.local>
2024-03-07 15:31:30 +0100

    module1-task76

--- js/script.js
+++ js/script.js
@@ -3404,3 +3404,64 @@
 //     closeModal();
 //   }, 4000);
 // }
+
+
+
+// module1-task76   Promise(es6)
+
+// promise - обещание работает с асинхронным кодом
+
+
+// console.log('запрос данных') // синхронный код
+
+// const req = new Promise(function(resolve, reject) {
+
+//   setTimeout(() => { // хуй пойми че ебать, таймер в таймере асинхронное пекло
+//     console.log('подготовка данных');
+  
+//     const product = {
+//       name: 'tv',
+//       price: 2000
+//     };
+  
+//     resolve(product);
+  
+//   }, 2000);
+
+// });
+
+// req.then((product) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       product.status = 'order';
+//       resolve(product);
+//     }, 2000);
+//   });
+// }).then(data => {
+//   data.modify = true;
+//   return data;
+// }).then(data => {
+//   console.log(data);
+// }).catch(() => {
+//   console.log('error');
+// }).finally(() => {
+//   console.log('finally')
+// });
+
+
+// const test = time => {
+//   return new Promise(resolve => {
+//     setTimeout(() => resolve(), time);
+//   });
+// };
+
+// // test(1000).then(() => console.log("1000"));
+// // test(2000).then(() => console.log("2000"));
+
+// Promise.all([test(1000), test(2000)]).then(() => {
+//   console.log('All');
+// });
+
+// Promise.race([test(1000), test(2000)]).then(() => {
+//   console.log('All');
+// });
