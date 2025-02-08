8948f323a710c339052de3daaa19a941326adb6a
Cheese <ricsi@ricsis-MacBook-Pro.local>
2025-02-08 19:30:53 +0100

    module1-task123

--- js/script.js
+++ js/script.js
@@ -6708,3 +6708,197 @@
 // localStorage.setItem('alex', serializedPeson);
 
 // console.log(JSON.parse(localStorage.getItem('alex')));
+
+
+
+
+// module1-task123 Регулярные выражения
+
+// new RegExp('pattern', 'flags'); // класичесский метод создания регулярного выражения
+// /patter/flags // используемый метод создания регулярного выражения
+
+// const ans = prompt('Введите ваше имя');
+
+// const reg = /n/ig;
+// flag i найти вне зависимости от регистра
+// flag g найти несколько вхождений
+// flag m включает многострочный режим
+
+// console.log(ans.search(reg)); // метод search ищет только первое совпадение
+// console.log(ans.match(reg)); // при использовании match получаем массив данных
+
+// const pass = prompt('Password');
+
+// console.log(pass.replace(/./g, "*")); // паттерном точкой все введенные символы при выводе сменяются звездочкой
+
+// console.log('12-34-56'.replace(/-/g, ':'));
+
+
+// методы обьекта регулярного выражения RegExp
+
+// const ans = prompt('Введите ваше число');
+
+// const reg = /\d/ig;
+// console.log(ans.match(reg)); // вернет либо правда либо ложь, если найдет в строке совпадение с паттерном
+
+// const str = 'My name is R2D2';
+
+// console.log(str.match(/\w\d\w\d/i));
+
+// \d class цифры
+// \w class буквы
+// \s class пробелы
+
+// \D class не числа/цифры
+// \W class не буквы
+
+
+// использование регулярных выражений в слайдере
+// const slides = document.querySelectorAll('.offer__slide'),
+//       slider = document.querySelector('.offer__slider'),
+//       prev = document.querySelector('.offer__slider-prev'),
+//       next = document.querySelector('.offer__slider-next'),
+//       total = document.querySelector('#total'),
+//       current = document.querySelector('#current'),
+//       slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+//       slidesField = document.querySelector('.offer__slider-inner'),
+//       width = window.getComputedStyle(slidesWrapper).width;
+
+// let slideIndex = 1;
+// let offset = 0;
+
+// if (slides.length < 10) {
+//   total.textContent = `0${slides.length}`;
+//   current.textContent = `0${slideIndex}`;
+// } else {
+//   total.textContent = slides.length;
+//   current.textContent = slideIndex;
+// }
+
+// slidesField.style.width = 100 * slides.length + '%';
+// slidesField.style.display = 'flex';
+// slidesField.style.transition = '0.5 all';
+
+// slidesWrapper.style.overflow = 'hidden';
+
+// slides.forEach(slide => {
+//   slide.style.width = width;
+// });
+
+// slider.style.position = 'relative';
+
+// const indicators = document.createElement('ol'),
+//       dots = [];
+// indicators.classList.add('carousel-indicators');
+// indicators.style.cssText = `
+//   position: absolute;
+//   right: 0;
+//   bottom: 0;
+//   left: 0;
+//   z-index: 15;
+//   display: flex;
+//   justify-content: center;
+//   margin-right: 15%;
+//   margin-left: 15%;
+//   list-style: none;
+// `;
+// slider.append(indicators);
+
+// for (let i = 0; i < slides.length; i++) {
+//   const dot = document.createElement('li');
+//   dot.setAttribute('data-slide-to', i + 1);
+//   dot.style.cssText = `
+//     box-sizing: content-box;
+//     flex: 0 1 auto;
+//     width: 30px;
+//     height: 6px;
+//     margin-right: 3px;
+//     margin-left: 3px;
+//     cursor: pointer;
+//     background-color: #fff;
+//     background-clip: padding-box;
+//     border-top: 10px solid transparent;
+//     border-bottom: 10px solid transparent;
+//     opacity: .5;
+//     transition: opacity .6 ease;
+//   `;
+//   if (i == 0) {
+//     dot.style.opacity = 1; 
+//   }
+//   indicators.append(dot);
+//   dots.push(dot);
+// }
+
+// function deleteNotDigits(str) {
+//   return +str.replace(/\D/g, '');
+// }
+
+// next.addEventListener('click', () => {
+//   if (offset == deleteNotDigits(width) * (slides -1)) {
+//     offset = 0;
+//   } else {
+//     offset += deleteNotDigits(width);
+//   }
+
+//   slidesField.style.transform = `translateX(-${offset}px)`;
+
+//   if (slideIndex == slides.length) {
+//     slideIndex = 1;
+//   } else {
+//     slideIndex++;
+//   }
+
+//   if (slides.length < 10) {
+//     current.textContent = `0${slideIndex}`;
+//   } else {
+//     current.textContent = slideIndex;
+//   }
+
+//   dots.forEach(dot => dot.style.opacity = '.5');
+//   dots[slideIndex - 1].style.opacity = 1;
+// });
+
+// prev.addEventListener('click', () => {
+//   if (offset = 0) {
+//     offset = deleteNotDigits(width) * (slides -1)
+//   } else {
+//     offset -= deleteNotDigits(width);
+//   }
+
+//   slidesField.style.transform = `translateX(-${offset}px)`;
+
+//   if (slideIndex == 1) {
+//     slideIndex = slides.length;
+//   } else {
+//     slideIndex--;
+//   }
+
+//   if (slides.length < 10) {
+//     current.textContent = `0${slideIndex}`;
+//   } else {
+//     current.textContent = slideIndex;
+//   }
+
+//   dots.forEach(dot => dot.style.opacity = '.5');
+//   dots[slideIndex - 1].style.opacity = 1;
+// });
+
+// dots.forEach(dot => {
+//   dot.addEventListener('click' , (e) => {
+//     const slideTo = e.target.getAttribute('data-slide-to');
+
+//     slideIndex = slideTo;
+//     offset = offset = deleteNotDigits(width) * (slideTo -1);
+
+//     slidesField.style.transform = `translateX(-${offset}px)`;
+
+//     if (slides.length < 10) {
+//       current.textContent = `0${slideIndex}`;
+//     } else {
+//       current.textContent = slideIndex;
+//     }
+
+//     dots.forEach(dot => dot.style.opacity = '.5');
+//     dots[slideIndex - 1].style.opacity = 1;
+//   });
+// });
