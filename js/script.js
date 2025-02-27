e8593644e6436400d4fd6aa73b6bb754ae5be327
Cheese <ricsi@ricsis-MacBook-Pro.local>
2025-02-27 18:01:48 +0100

    module1-task127

--- js/script.js
+++ js/script.js
@@ -7117,3 +7117,72 @@
 
 // console.log(person.userAge = 30);
 // console.log(person.userAge);
+
+
+
+
+// module1-task127 инкапсуляция
+
+// function User(name, age) {
+//   this.name = name;
+//   let userAge = age;
+
+//   this.say = function() {
+//     console.log(`Name of the user: ${this.name}, age ${userAge}`);
+//   };
+
+//   this.getAge = function() {
+//     return userAge;
+//   };
+
+//   this.setAge = function(age) {
+//     if (typeof age === 'number' && age > 0 && age < 110) {
+//       userAge = age;
+//     } else {
+//       console.log('unverified value');
+//     }
+//   };
+// }
+
+// const ivan = new User('Ivan', 27);
+// console.log(ivan.name);
+// console.log(ivan.getAge());
+
+// ivan.setAge(30);
+// ivan.setAge(300);
+// console.log(ivan.getAge());
+
+// ivan.say();
+
+
+// на классах
+
+// class User {
+//   constructor(name, age) {
+//     this.name = name;
+//     this._age = age;
+//   }
+
+//   #surname = 'Petrychencko';
+
+//   say = () => {
+//     console.log(`Name of the user: ${this.name}${this.#surname}, age ${this._age}`);
+//   }
+
+//   get age() {
+//     return this._age;
+//   }
+
+//   set age(age) {
+//     if (typeof age === 'number' && age > 0 && age < 110) {
+//       this._age = age;
+//     } else {
+//       console.log('unverified value');
+//     }
+//   }
+// }
+
+// const ivan = new User('Ivan', 27);
+// console.log(ivan.surname);
+
+// ivan.say();
