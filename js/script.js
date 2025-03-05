95c18c4dee76aaf5c3e5ec83596c84df580e2cb4
Cheese <ricsi@ricsis-MacBook-Pro.local>
2025-03-05 10:47:56 +0100

    module1-task138

--- js/script.js
+++ js/script.js
@@ -7425,3 +7425,53 @@
 //     }, 2000);
 //   });
 // });
+
+
+
+
+// module1-task138 функции генераторы
+
+// function* generator() {
+//   yield 'S';
+//   yield 'c';
+//   yield 'r';
+//   yield 'i';
+//   yield 'p';
+//   yield 't';
+// }
+
+// const str = generator();
+
+// // console.log(str.next());
+// // console.log(str.next());
+// // console.log(str.next());
+// // console.log(str.next());
+// // console.log(str.next());
+// // console.log(str.next());
+// // console.log(str.next());
+
+// console.log(str.next().value);
+
+
+// function* count(n) {
+//   for (let i = 0; i < n; i++) {
+//     yield i;
+//   }
+// }
+
+// const counter = count(7);
+
+// console.log(counter.next().value);
+// console.log(counter.next().value);
+// console.log(counter.next().value);
+
+
+// function* count(n) {
+//   for (let i = 0; i < n; i++) {
+//     yield i;
+//   }
+// }
+
+// for (let k of count(7)) {
+//   console.log(k);
+// }
